814f8417f8b97e4260cf1efce7f1386f130431d8
shreevidyamatabhuvanesvari-gif <shreevidyamatabhuvanesvari@gmail.com>
2026-02-16 21:42:56 +0530

    Update knowledge_v2.js

--- js/knowledge_v2.js
+++ js/knowledge_v2.js
@@ -5,9 +5,31 @@
    Wikipedia Powered (Robust Topic Extraction)
    ====================================== */
 
-var KnowledgeEngineV2 = (function () {
+var KnowledgeEngine = (function () {
 
-    const API = "https://hi.wikipedia.org/api/rest_v1/page/summary/";
+    async function search(topic) {
+        try {
+            var url = "https://hi.wikipedia.org/api/rest_v1/page/summary/" + encodeURIComponent(topic);
+            var res = await fetch(url);
+
+            if (!res.ok) return null;
+
+            var data = await res.json();
+
+            if (data.extract) {
+                return data.extract;
+            }
+
+        } catch (e) {
+            return null;
+        }
+
+        return null;
+    }
+
+    return {
+        search: search
+    }
 
     /* ---------- Clean Topic ---------- */
     function cleanTopic(text) {
